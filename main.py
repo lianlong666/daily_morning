2c35030c1818bdb189c3ccac79b6b0e8ce85352a
lianlong <2801891720@qq.com>
2022-08-22 19:05:27 +0800

    1

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp']), math.floor(weather['wind'])
+  return weather['weather'], math.floor(weather['temp']), weather['wind']
 
 # 在一起时间
 def get_count():
